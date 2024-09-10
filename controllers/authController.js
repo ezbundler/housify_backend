9331cc15242805e8f64e8f2c3331c7202c07849d
ezbundler <developer.akash1236@gmail.com>
2024-09-10 19:44:41 +0530

    added the error controller to send customized error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,24 @@
 const User = require('../models/userModel')
 const bcryptjs  = require('bcryptjs');
+const errorHandler = require('../utils/error.js');
 
-const signup =async (req,res)=>{
+const signup =async (req,res,next)=>{
 const{username,  email, password}=req.body;
 const  hashedPassword =bcryptjs.hashSync(password, 10);
 console.log('yyyyyyyyyyyyyyyyyy',req.body);
 
+
 const user = new User({username,email,password:hashedPassword});
-await user.save();
+
+try {
+    await user.save();
 console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',user)
 res.status(201).json({message:"User created successfully"})
+} catch (error) {
+//    next(error);
+    next(errorHandler(550,`you are getting the error from error util, error:${error} `));
+}
+
 
     
 } 
