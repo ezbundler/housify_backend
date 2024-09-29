f442690ee9ce6a26536ab130018d1f2529e65ed5
ezbundler <developer.akash1236@gmail.com>
2024-09-29 12:17:36 +0530

    search API

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,3 +1,4 @@
+const { parse } = require("dotenv");
 const Listing = require("../models/listingModel.js");
 const errorHandler = require("../utils/error.js");
 
@@ -60,4 +61,53 @@ const fetchListing = async(req,res,next)=>{
       next(error);
    }
 }
-module.exports = {createListing,deleteListing,updateListing,fetchListing};
+
+
+const getListings = async(req,res,next)=>{
+   try {
+      const limit = parseInt( req.query.limit) || 10;
+const startIndex = parseInt(req.query.startIndex)||0;
+let offer = req.query.offer;
+if(offer ===undefined|| offer ==='false'){
+   offer = {$in: [false, true]};
+}
+
+let furnished = req.query.furnished;
+if(furnished===  undefined|| furnished==='false'){
+   furnished = {$in: [false, true]};
+}
+
+let parking = req.query.parking;
+if(parking=== undefined|| parking==='false'){
+   parking = {$in: [false, true]};
+}
+
+let type = req.query.type;
+if(type=== undefined|| type==='all'){
+   type = {$in: ['sale', 'rent']};
+}
+
+const searchTerm = req.query.searchTerm || '';
+
+const sort = req.query.sort || 'createdAt';
+
+const order = req.query.order || 'desc';
+
+
+const listings = await  Listing.find({
+   name:{$regex: searchTerm,  $options: 'i'},
+   offer,
+   furnished,
+   parking,
+   type,
+
+}).sort({[sort]: order}).limit(limit).skip(startIndex);
+return res.status(200).json(listings);
+
+
+   } catch (error) {
+      next(error)
+   }
+
+}
+module.exports = {getListings,createListing,deleteListing,updateListing,fetchListing};
--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createListing, deleteListing,updateListing,fetchListing}= require('../controllers/listingController.js');
+const {createListing,getListings, deleteListing,updateListing,fetchListing}= require('../controllers/listingController.js');
 const verifyToken = require('../utils/verifyUser.js');
 
 
@@ -11,4 +11,5 @@ router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
 router.post('/update/:id',verifyToken, updateListing);
 router.get('/get/:id',fetchListing);
+router.get('/get',getListings)
 module.exports = router;
